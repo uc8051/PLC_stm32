798e8fb5468b09a7a4b22aa3477a436f2e0545e9
maswi <maswi@NPL-NB0114.npl.lan.noratel.no>
2026-09-23 13:31:30 +0200

    działa min

--- Core/Src/plc.c
+++ Core/Src/plc.c
@@ -22,11 +22,25 @@ void PLC_Run(void)
      */
 
     if (IN(1)) {
-        OUT(3, ON);
+        OUT(1, ON);
     } else {
-        OUT(3, OFF);
+        OUT(1, OFF);
     }
 
+    if (IN(2)) {
+        OUT(2, ON);
+        OUT(3, ON);
+        OUT(4, ON);
+        OUT(5, ON);
+        OUT(6, ON);
+    } else {
+        OUT(2, OFF);
+        OUT(3, OFF);
+        OUT(4, OFF);
+        OUT(5, OFF);
+        OUT(6, OFF);
+
+    }
 
 
 }
